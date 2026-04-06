4ba7f3ed3b2482d57058ba2a947796b4fafea77f
MB-PRASANNAKUMAR <23csea53prasannakumar.m.b@gmail.com>
2026-04-06 07:06:33 +0530

    updated app.py

--- app.py
+++ app.py
@@ -24,6 +24,13 @@ app.secret_key = os.environ.get("SECRET_KEY", "dev-secret")
 RESUME_FOLDER = "/tmp/uploads"
 os.makedirs(RESUME_FOLDER, exist_ok=True)
 
+# Check if database exists
+if not os.path.exists(DB_NAME):
+    print("Database not found. Initializing...")
+    init_db(DB_NAME)
+else:
+    print("Database already exists. Skipping initialization.")
+    
 # ---------- DATABASE ----------
 def db():
     con = sqlite3.connect(DB_NAME)
